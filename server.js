658f0fe7a03335f7b51d99fffffb27286d7a714e
Manish Kumar <manishkumarbhadhala@gmail.com>
2026-07-13 10:07:07 +0530

    create db connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const pool = require('./config/db');
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.get('/', (req, res) => {
   res.send('URL Shortener API is running...');
 });
 
+app.get('/test-db', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT 1 + 1 AS result');
+    res.json({ message: 'DB connected successfully', result: rows[0].result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'DB connection failed', error: error.message });
+  }
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
